f17681e18885404bb5c3c3f95bd120ffe1de6475
Leesin0222 <dragon02229@naver.com>
2022-06-02 02:20:40 +0900

    ⚡ :: jetpackCompose BasicLayout  맹들음

---
+++ app/src/main/java/com/company/layoutbasic/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.company.layoutbasic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.company.layoutbasic.ui.theme.LayoutbasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutbasicTheme {
+                UserCard()
+            }
+        }
+    }
+}
+
+@Composable
+fun UserCard() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(12.dp)
+            .border(1.dp, color = Color.Gray)
+            .padding(12.dp)
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.jungmin),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape))
+        Column() {
+            Text(text = stringResource(id = R.string.explan_text))
+            Button(onClick = {
+
+            }) {
+                Text(text = "프로필 보기")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface(Modifier.fillMaxSize()) {
+        UserCard()
+    }
+}
